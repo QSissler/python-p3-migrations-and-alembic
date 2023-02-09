aa0729a3a8754badc147ad6a74f77d0d23397edb
Quinn Sissler <QSissler@gmail.com>
2023-02-09 13:42:13 -0500

    completed lab

---
+++ lib/migrations/versions/fc8f4aa544f3_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: fc8f4aa544f3
+Revises: 
+Create Date: 2023-02-09 13:33:00.253075
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fc8f4aa544f3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
